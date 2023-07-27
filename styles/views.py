c5a0b51dc2fc6921fe1e5f59933ecf32ce6cce84
McCleavage <stuartmccleave@gmail.com>
2023-07-27 08:30:52 +0200

    Adding docstrings to styles/views.py

--- styles/views.py
+++ styles/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Style
-# Create your views here.
+
+
 def index(request):
+    """This function is used to render the index page of the dances
+
+        :param WSGIRequest request: The web server request
+        :returns: The rendered index page
+
+        :rtype: HttpResponse
+    """
     return render(request, "index.html")
 
-# Locks viewing of individual dance pages to logged-in users
+
 def detail(request, style_id):
+    """This function is used to render views of individual dance pages to authenticated users
+        or redirect to login page otherwise
+
+        :param WSGIRequest request: The web server request
+        :param int style_id: The id of the style requested
+        :returns: A redirect to either the login or home page, depending on if authenticated
+
+        :rtype: HttpResponse
+    """
     if request.user.is_authenticated:
         style = get_object_or_404(Style, pk=style_id)
         return render(request, "style.html", {'style': style})

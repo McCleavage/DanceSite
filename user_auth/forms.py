d85f604e8784338144eb9ec89b69f8f59acc43de
McCleavage <stuartmccleave@gmail.com>
2023-07-27 08:22:48 +0200

    Added docstrings to forms.py

--- user_auth/forms.py
+++ user_auth/forms.py
@@ -2,10 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-# User registation form, simply containing username and password, but added first_name field
+
 class UserRegistrationForm(UserCreationForm):
+    """This class represents an extension of the stock Django user form
+
+        :param CharField first_name: The user's first name
+        :param class Meta: Meta class data storing user model and all fields
+
+    """
     first_name = forms.CharField(max_length=120)
 
     class Meta:
+        """This class represents some of the metadata of a user
+
+            :param User model: The model of the user
+            :param list fields:List referring to all fields
+
+        """
         model = User
         fields = ['username', 'first_name', 'password1', 'password2']
